b6f62e1793f4711e167a96cfbb903f8931195154
Bastien <bmoulia@gmail.com>
2025-02-04 21:45:49 +0100

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "worx-time",
+        appId: "1:635974150643:web:c7db67b2b041cb2d710a86",
+        storageBucket: "worx-time.firebasestorage.app",
+        apiKey: "AIzaSyB0aE4sIEfKPSce7Ge1DLueXu-gbH-l4jQ",
+        authDomain: "worx-time.firebaseapp.com",
+        messagingSenderId: "635974150643",
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
